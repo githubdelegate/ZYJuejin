af5e9ba544211a8eb4fa398a8611937c672eeee7
githubdelegate <ostmail@163.com>
2018-10-18 15:33:13 +0800

    添加沸点推荐

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -1,53 +1,93 @@
 // pages/hot/hot.js
+const config = getApp().globalData.config
+const utils = require('../../util/util')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    COUNT: 30,
+    swiperHeight: 'auto',
+    recommendList: [],
+    list: [],
+    auth: {},
+    scrollTop: 0
+    },
 
+  // 获取沸点 列表
+  getHotRecommendList () {
+    const auth = this.data.auth
+    wx.request({
+      url: `${config.shortMsgMsRequestUrl}/getHotRecommendList`,
+      data: {
+        uid: auth.uid,
+        device_id: auth.clientId,
+        client_id: auth.client_id,
+        token: auth.token,
+        src: 'web',
+      },
+      success: (result)=>{
+        let data = result.data
+        if (data.s === 1) {
+          this.setData({
+            recommendList: (data.d && data.d.list) || [],
+          })
+        }else {
+          wx.showToast({
+            title: data.m.toString(),
+            icon: 'none'
+          })
+        }
+      },
+      fail: ()=>{
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none',
+        });
+      },
+      complete: ()=>{
+        wx.stopPullDownRefresh()
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  initSwiper () {
+    wx.getSystemInfo({
+      success: (result)=>{
+        this.setData({
+          swiperHeight: `${(result.windowWidth || result.screenWidth) / 375 * 135}px`
+        })
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  init () {
+    this.setData({
+      auth: {},
+    })
+    this.initSwiper()
+    this.getHotRecommendList()
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    if (utils.isEmptyObj(this.data.recommendList)) {
+      wx.startPullDownRefresh({
+      });
+    }
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.init()
   },
 
   /**
